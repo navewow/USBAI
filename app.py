6089083c5cb66a9713031ed558580dbb613d88c6
AlaguSaranya94 <AlaguSaranya.M@cognizant.com>
2016-11-25 14:49:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,20 +20,7 @@ def webhook():
 	data = request.get_json()
 	print data
 	return "Welcome"
-##	json_data_final='{"recipient": {"id": sender_id}, "message": {"text": out_msg}}'
-##	req = urllib2.Request('https://graph.facebook.com/v2.6/me/messages?access_token=EAAZAgx2FZBzKoBANeLcN8OcYJ8UZApCZBXYjpO29pZC4rQ2xGuJLdBHzmgOOkD22efYJn1OmdAUvYlWBNYZApOjLO167WH0UIA7jYvqsPy3FTKdn4ZBMZAWlenyYFdhKVu5PjzzESrB5wmjXwTZBjThJ2PVhGJbL0CrOJZBI1S82kWlFqMe7de4GRl')
-##	req.add_header('Content-Type','application/json')
-####	response = urllib2.urlopen(req, json.dumps(json_data_final))
-##	response = urllib2.urlopen(req)
-##	return '123'
-##postingMessage(json_data_final);
-                
 
-##def postingMessage(json_data):
- #       req = urllib2.Request('https://graph.facebook.com/v2.6/me/messages?access_token=EAAZAgx2FZBzKoBANeLcN8OcYJ8UZApCZBXYjpO29pZC4rQ2xGuJLdBHzmgOOkD22efYJn1OmdAUvYlWBNYZApOjLO167WH0UIA7jYvqsPy3FTKdn4ZBMZAWlenyYFdhKVu5PjzzESrB5wmjXwTZBjThJ2PVhGJbL0CrOJZBI1S82kWlFqMe7de4GRl')
- #       req.add_header('Content-Type', 'application/json')
-  #      response = urllib2.urlopen(req, json.dumps(json_data))
-   #     return response;
 
 if __name__ == '__main__':
         app.run()
