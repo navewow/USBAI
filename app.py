0833fbd82ad01f4fee1c3edd8bff809a403f9e4b
AlaguSaranya94 <AlaguSaranya.M@cognizant.com>
2016-12-02 15:51:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def webhook():
 
 	# endpoint for processing incoming messaging events
 	data = request.get_json()
-	print data
+##	print data
 	log(data)  # you may not want to log every incoming message in production, but it's good for testing
 ##	value=request.data
 ##	jsonResponse=json.loads(value)
@@ -41,7 +41,7 @@ def webhook():
 					sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
 					recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
 					message_text = messaging_event["message"]["text"]  # the message's text
-##				if "block" in message_text:
+					print message_text
 					send_message(sender_id, "you?")
 	
 
