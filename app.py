26cf49950b2edb5f4500603d48e0d56276b4720c
AlaguSaranya94 <alagusaranya.m@cognizant.com>
2016-12-09 16:34:25 +0530

    Plain

--- app.py
+++ app.py
@@ -76,29 +76,8 @@ def send_message(recipient_id, message_text):
                     "type": "template",
                     "payload": {
                         "template_type": "list",
+                        "top_element_style": "compact",
                         "elements": [
-                            {
-                                "title": "Classic T-Shirt Collection",
-                                "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
-                                "subtitle": "See all our colors",
-                                "default_action": {
-                                    "type": "web_url",
-                                    "url": "https://peterssendreceiveapp.ngrok.io/shop_collection",
-                                    "messenger_extensions": true,
-                                    "webview_height_ratio": "tall",
-                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                                },
-                                "buttons": [
-                                    {
-                                        "title": "View",
-                                        "type": "web_url",
-                                        "url": "https://peterssendreceiveapp.ngrok.io/collection",
-                                        "messenger_extensions": true,
-                                        "webview_height_ratio": "tall",
-                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                                    }
-                                ]
-                            },
                             {
                                 "title": "Classic White T-Shirt",
                                 "image_url": "https://peterssendreceiveapp.ngrok.io/img/white-t-shirt.png",
@@ -112,7 +91,7 @@ def send_message(recipient_id, message_text):
                                 },
                                 "buttons": [
                                     {
-                                        "title": "Shop Now",
+                                        "title": "Buy",
                                         "type": "web_url",
                                         "url": "https://peterssendreceiveapp.ngrok.io/shop?item=100",
                                         "messenger_extensions": true,
@@ -134,7 +113,7 @@ def send_message(recipient_id, message_text):
                                 },
                                 "buttons": [
                                     {
-                                        "title": "Shop Now",
+                                        "title": "Buy",
                                         "type": "web_url",
                                         "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
                                         "messenger_extensions": true,
@@ -156,9 +135,31 @@ def send_message(recipient_id, message_text):
                                 },
                                 "buttons": [
                                     {
-                                        "title": "Shop Now",
+                                        "title": "Buy",
                                         "type": "web_url",
                                         "url": "https://peterssendreceiveapp.ngrok.io/shop?item=102",
+                                        "messenger_extensions": true,
+                                        "webview_height_ratio": "tall",
+                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                    }
+                                ]
+                            },
+                            {
+                                "title": "Classic Gray T-Shirt",
+                                "image_url": "https://peterssendreceiveapp.ngrok.io/img/gray-t-shirt.png",
+                                "subtitle": "100% Cotton, 200% Comfortable",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                                    "messenger_extensions": true,
+                                    "webview_height_ratio": "tall",
+                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                },
+                                "buttons": [
+                                    {
+                                        "title": "Buy",
+                                        "type": "web_url",
+                                        "url": "https://peterssendreceiveapp.ngrok.io/shop?item=103",
                                         "messenger_extensions": true,
                                         "webview_height_ratio": "tall",
                                         "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
