4ddcefd73a23b3835cb2b6fb0342a8012492fd94
AlaguSaranya94 <alagusaranya.m@cognizant.com>
2016-12-09 18:13:22 +0530

    revert

--- app.py
+++ app.py
@@ -73,100 +73,61 @@ def send_message(recipient_id, message_text):
             },
             "message": {
                 "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "list",
-                        "top_element_style": "compact",
-                        "elements": [
-                            {
-                                "title": "Classic White T-Shirt",
-                                "image_url": "https://peterssendreceiveapp.ngrok.io/img/white-t-shirt.png",
-                                "subtitle": "100% Cotton, 200% Comfortable",
-                                "default_action": {
-                                    "type": "web_url",
-                                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
-                                    "webview_height_ratio": "tall",
-                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                                },
-                                "buttons": [
-                                    {
-                                        "title": "Buy",
-                                        "type": "web_url",
-                                        "url": "https://peterssendreceiveapp.ngrok.io/shop?item=100",
-                                        "webview_height_ratio": "tall",
-                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                                    }
-                                ]
-                            },
-                            {
-                                "title": "Classic Blue T-Shirt",
-                                "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                                "subtitle": "100% Cotton, 200% Comfortable",
-                                "default_action": {
-                                    "type": "web_url",
-                                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                                    "webview_height_ratio": "tall",
-                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                                },
-                                "buttons": [
-                                    {
-                                        "title": "Buy",
-                                        "type": "web_url",
-                                        "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                                        "webview_height_ratio": "tall",
-                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                                    }
-                                ]
-                            },
-                            {
-                                "title": "Classic Black T-Shirt",
-                                "image_url": "https://peterssendreceiveapp.ngrok.io/img/black-t-shirt.png",
-                                "subtitle": "100% Cotton, 200% Comfortable",
-                                "default_action": {
-                                    "type": "web_url",
-                                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=102",
-                                    "webview_height_ratio": "tall",
-                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                                },
-                                "buttons": [
-                                    {
-                                        "title": "Buy",
-                                        "type": "web_url",
-                                        "url": "https://peterssendreceiveapp.ngrok.io/shop?item=102",
-                                        "webview_height_ratio": "tall",
-                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                                    }
-                                ]
-                            },
-                            {
-                                "title": "Classic Gray T-Shirt",
-                                "image_url": "https://peterssendreceiveapp.ngrok.io/img/gray-t-shirt.png",
-                                "subtitle": "100% Cotton, 200% Comfortable",
-                                "default_action": {
-                                    "type": "web_url",
-                                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-                                    "webview_height_ratio": "tall",
-                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                                },
-                                "buttons": [
-                                    {
-                                        "title": "Buy",
-                                        "type": "web_url",
-                                        "url": "https://peterssendreceiveapp.ngrok.io/shop?item=103",
-                                        "webview_height_ratio": "tall",
-                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                                    }
-                                ]
-                            }
-                        ],
-                         "buttons": [
-                            {
-                                "title": "View More",
-                                "type": "postback",
-                                "payload": "payload"
-                            }
-                        ]
+                  "type":"template",
+                  "payload":{
+                    "template_type":"receipt",
+                    "recipient_name":"Stephane Crozatier",
+                    "order_number":"12345678902",
+                    "currency":"USD",
+                    "payment_method":"Visa 2345",
+                    "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
+                    "timestamp":"1428444852",
+                    "elements":[
+                      {
+                        "title":"U.S. Bank",
+                        "subtitle":"BAL @ 12/02/16 9:28am CT\nCustomer's Account In A4: $382.57 \nSavings A6: $655.63",
+                        "quantity":1,
+                        "price":0,
+                        "currency":"USD",
+                        "image_url":"http://petersapparel.parseapp.com/img/whiteshirt.png"
+                      },
+                      {
+                        "title":"Transaction History",
+                        "subtitle":"A4 at 12/02/16 9:28am CT",
+                        "quantity":1,
+                        "price":0,
+                        "currency":"USD",
+                        "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
+                      },
+                      {
+                        "title":"12/01/16 Web Author",
+                        "subtitle":"Debit",
+                        "quantity":1,
+                        "price":159.90,
+                        "currency":"USD",
+                        "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
+                      },
+                      {
+                        "title":"12/01/16 Debit Purc",
+                        "subtitle":"Debit",
+                        "quantity":1,
+                        "price":19.98,
+                        "currency":"USD",
+                        "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
+                      },
+                      {
+                        "title":"12/02/16 Electronic",
+                        "subtitle":"Credit",
+                        "quantity":1,
+                        "price":856.45,
+                        "currency":"USD",
+                        "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
+                      }
+                    ],
+                    "balance":{
+                      "total_cost":756.14
                     }
+                  }
                 }
             }
         })
