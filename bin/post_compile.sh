ca252ba14610d78061ba275a41eef14c4fd82549
rajamario <raja.mario@cognizant.com>
2016-12-14 16:55:07 +0530

    cc

--- bin/post_compile.sh
+++ bin/post_compile.sh
@@ -5,10 +5,5 @@ echo "-----> Running install_nltk_data"
 chmod +x bin/install_nltk_data
 bin/install_nltk_data
 
-##if [ -f bin/install_nltk_data ]; then
-    ##echo "-----> Running install_nltk_data"
-    ##chmod +x bin/install_nltk_data
-    ##bin/install_nltk_data
-##fi
 
 echo "-----> Post-compile done"
