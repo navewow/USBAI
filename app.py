5fd03ee64aad1d0498c30836182bc21c31af365f
rajamario <raja.mario@cognizant.com>
2016-12-15 12:17:21 +0530

    sdfgsdfg

--- app.py
+++ app.py
@@ -119,17 +119,17 @@ def send_message(recipient_id, message_text):
                     "buttons":[
                       {
                         "type":"postback",
-                        "title":"12/01/16 Web Author" + " -$159.90",
+                        "title":"12/01/16 Web Author" + " $159.90",
                         "payload":"Main Menu"
                       },
                       {
                         "type":"postback",
-                        "title":"12/01/16 Debit Purc" +" -$19.98",
+                        "title":"12/01/16 Debit Purc" +" $19.98",
                         "payload":"Main Menu"
                       },
                       {
                         "type":"postback",
-                        "title":"12/02/16 Electronic" + "+$856.45",
+                        "title":"12/02/16 Electronic" + " $856.45 Cr",
                         "payload":"Main Menu"
                       },
                       {
